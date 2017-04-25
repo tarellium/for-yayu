19621ba72369f30d6b43485fea125d69f96befce
tarellium <tarellium1@gmail.com>
2017-04-25 01:54:33 -0400

    YAY

---
+++ selenium/src/selenium/YA2.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class YA2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+/*FirefoxDriver driver = new FirefoxDriver();*/
+System.out.println("this is the change");
+
+	}
+
+}
---
+++ selenium/src/selenium/Yayu.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Yayu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new selenium.WebDriver();
+		
+
+	}
+
+}
